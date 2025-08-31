b6e87e11bc697fafcc82a5a100e29f67d4492996
Praneeth13-2004 <karthikeyarsp@gmail.com>
2025-08-31 12:30:04 +0530

    Update langchain_ollama.py

--- langchain_ollama.py
+++ langchain_ollama.py
@@ -1,10 +1,34 @@
+
 import os
 from langchain_community.chat_models import ChatOllama
 import streamlit as st
+from langchain.prompts import PromptTemplate
+
 
 llm = ChatOllama(model="llama3")
-st.title("Ask anything")
-question = st.text_input("enter question: ")
-if question:
-    response = llm.invoke(question)
+prompt_template = PromptTemplate(
+    input_variables = ["company", "position",
+                       "strengths", "weaknesses"
+                       ],
+    template = """
+        You are a career coach. Provide tailored interview tips for the position of {position} at {company}.
+        Highlight your Strengths in {strengths} and prepare for question.
+        about your weaknesses as {weaknesses}.
+    """
+)
+st.title("Interview Tutor")
+company = st.text_input("Company Name: ")
+position = st.text_input("Position Title: ")
+strengths = st.text_area("Your Strengths", height=100)
+weaknesses = st.text_area("Your Weaknesses",height=100)
+
+if company and position and strengths and weaknesses:
+    response = llm.invoke(prompt_template.format(
+        company = company,
+        position = position,
+        strengths = strengths,
+        weaknesses = weaknesses
+    ))
     st.write(response.content)
+
+
